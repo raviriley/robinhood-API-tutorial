c7bb48d909c9e9dc8aead458b79757bdd3e5dacf
Ravi <raviriley@gmail.com>
2020-07-29 14:10:50 -0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,17 +11,25 @@ pswd = "example"
 
 rh.login(user, pswd)
 
-stock_ticker = "TSLA"
+stock_ticker = "FB"
 dollar_amount = 1
 
 stock_name = rh.stocks.get_instruments_by_symbols(stock, info="simple_name")
     
-print(stock_name + " stock price: " + rh.stocks.get_quotes(stock, info="last_trade_price")[0]) # accessing the first element of the returned list
+print(stock_name + " stock price: " + rh.stocks.get_quotes(stock, info="last_trade_price")[0])
 
-if (rh.stocks.get_instruments_by_symbols(stock, info="fractional_tradability")[0] == 'tradable'):
-    print("Attempting to buy")
-    print(rh.orders.order_buy_fractional_by_price(stock, dollar_amount))
+if (rh.stocks.get_instruments_by_symbols(stock_ticker, info="fractional_tradability")[0] == 'tradable'):
+    print("Fractional trading is enabled for " + stock_ticker)
+    print("Attempting to buy $",dollar_amount," of "+stock_ticker)
+    #print(rh.orders.order_buy_fractional_by_price(stock_ticker, dollar_amount, 'gfd')) # buy $1 of stock_ticker, order good for day
+    order_ID = rh.orders.get_all_stock_orders(info='id')[0] # get ID of latest order
+    purchased_price = float(rh.orders.get_stock_order_info(order_ID)['average_price'])
+    purchased_quantity = float(rh.orders.get_stock_order_info(order_ID)['quantity'])
+    while True: # this is bad practice because we will make tons of API calls, but this is just a simple demo so it's okay
+        current_dollar_value = float(rh.stocks.get_quotes(stock_ticker, info="last_trade_price")[0])*purchased_quantity
+        if ( current_dollar_value >= 1.01 ):
+            print("Attempting to sell ",current_dollar_value," of "+stock_ticker)
+            rh.orders.order_sell_fractional_by_quantity(stock_ticker, purchased_quantity, 'gfd')
 else: 
-    print(stock_ticker + " isn't fractionally tradeable. Try a different stock.")
+    print(stock_ticker+" isn't fractionally tradeable")
 
-#need to get selling and pricing info
